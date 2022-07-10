36ee697f2cf8063e12e133ef845becaae3a6b242
Daniel Maldonado <damaldy19@hotmail.com>
2022-07-10 17:12:19 -0500

    app: createBoard fn created

--- app.js
+++ app.js
@@ -59,5 +59,24 @@ const cardArray = [
 
 cardArray.sort(() =>  0.5 - Math.random());
 /*.sort() method needs 2 values for ordering the array, each one assigned to each element -> In this case, the first elm has a value of 0.5 and the second elm has a random value btw 0 & 1 -> if the second value is lower to 0.5, then it will go last, but if is higher to 0.5, then it will be first among the 2 values -> Thats how the array will be randomly ordered.*/ 
-console.log(cardArray);
-//
+
+
+//4.Create the boardgame.
+
+const gridDisplay = document.querySelector('#grid');
+/*.querySelector() search into the document for the elm that has id as grid -> capture it in the const.*/
+
+//4.1.Create an image for each card we need.
+
+    function createBoard() {
+        for( let i = 0; i < cardArray.length; i++) {
+            const card = document.createElement('img');
+            /*.createElement does what the name says. The element type is defined in the param of the method*/
+            card.setAttribute('src', 'cardArray.image');
+            card.setAttribute('data-id', i);
+            console.log(card, i);
+            //For keeping track of each img.
+        }
+    };
+
+    createBoard();
