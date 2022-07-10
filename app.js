fb8f182e8e2cffecb0a1073aa90e903f19560f68
Daniel Maldonado <damaldy19@hotmail.com>
2022-07-10 16:34:25 -0500

    app: cards created

---
+++ app.js
@@ -0,0 +1,55 @@
+//1.Create the array that will contain the cards.
+
+const cardArray = [
+    {
+        name: 'cheeseburger',
+        img: './Images/cheeseburger.png',
+    },
+    {
+        name: 'fries',
+        img: './Images/fries.png',
+    },
+    {
+        name: 'hotdog',
+        img: './Images/hotdog.png',
+    },
+    {
+        name: 'ice-cream',
+        img: './Images/ice-cream.png',
+    },
+    {
+        name: 'milkshake',
+        img: './Images/milkshake.png',
+    },
+    {
+        name: 'pizza',
+        img: './Images/pizza.png',
+    },
+
+    //2.Put their pair cards.
+
+    {
+        name: 'cheeseburger',
+        img: './Images/cheeseburger.png',
+    },
+    {
+        name: 'fries',
+        img: './Images/fries.png',
+    },
+    {
+        name: 'hotdog',
+        img: './Images/hotdog.png',
+    },
+    {
+        name: 'ice-cream',
+        img: './Images/ice-cream.png',
+    },
+    {
+        name: 'milkshake',
+        img: './Images/milkshake.png',
+    },
+    {
+        name: 'pizza',
+        img: './Images/pizza.png',
+    },
+]
