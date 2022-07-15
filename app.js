a74c1ce018a9939d674fa7006dc3d0b3e606bc18
Daniel Maldonado <damaldy19@hotmail.com>
2022-07-14 20:46:27 -0500

    app: notes modified

--- app.js
+++ app.js
@@ -58,10 +58,10 @@ const cardArray = [
 //3.Sort the cards randomly.
 
 cardArray.sort(() =>  0.5 - Math.random());
-/*.sort() method needs 2 values for ordering the array, each one assigned to each element -> In this case, the first elm has a value of 0.5 and the second elm has a random value btw 0 & 1 -> if the second value is lower to 0.5, then it will go last, but if is higher to 0.5, then it will be first among the 2 values -> Thats how the array will be randomly ordered.*/ 
+/*.sort() method needs 2 values for ordering the array, each one assigned to each array element -> In this case, the first elm has a value of 0.5 and the second elm has a random value btw 0 & 1 -> if the second value is lower to 0.5, then it will go last, but if is higher to 0.5, then it will be first among the 2 values -> Thats how the array will be randomly ordered.*/ 
 
 
-//4.Create the boardgame.
+//4.Create the board for the game.
 
 const gridDisplay = document.querySelector('#grid');
 /*.querySelector() search into the document for the elm that has id as grid -> capture it in the const.*/
@@ -77,7 +77,7 @@ const gridDisplay = document.querySelector('#grid');
             card.setAttribute('data-id', i);
             //For keeping track of each img.
             gridDisplay.appendChild(card);
-            /*.appendChild sets card node as a child (& will be contained) in grindDisplay node. NOTE:everything in html is a node.*/
+            /*.appendChild sets card node as a child (& will be contained) in grindDisplay node. NOTE:everything in html is a node. In others terms, it says card are childs of gridDisplay.*/
         }
     };
 
